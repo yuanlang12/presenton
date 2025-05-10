7cd0db2716defcaade459765e988a486fa10d647
sauravniraula <developmentsaurav@gmail.com>
2025-05-10 23:38:46 +0545

    Removes: pdf2image as python dependency

--- servers/fastapi/image_processor/utils.py
+++ servers/fastapi/image_processor/utils.py
@@ -1,11 +1,16 @@
 import asyncio
+import os
 from api.services.instances import temp_file_service
-from pdf2image import convert_from_path
+import pdfplumber
 
 
 def get_page_images_from_pdf(document_path: str, temp_dir: str):
     images_temp_dir = temp_file_service.create_dir_in_dir(temp_dir)
-    return convert_from_path(document_path, output_folder=images_temp_dir)
+
+    with pdfplumber.open(document_path) as pdf:
+        for page in pdf.pages:
+            img = page.to_image()
+            img.save(os.path.join(images_temp_dir, f"page_{page.page_number}.png"))
 
 
 async def get_page_images_from_pdf_async(document_path: str, temp_dir: str):
