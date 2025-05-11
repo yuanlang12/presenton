2ca22b9e770fea6a064303b63f4906d304ac1fe0
sauravniraula <developmentsaurav@gmail.com>
2025-05-12 01:12:41 +0545

    Changes: auto-clean tempdir, loading animation in homepage

--- app/constants.ts
+++ app/constants.ts
@@ -9,7 +9,7 @@ export const baseDir = app.getAppPath();
 export const fastapiDir = isDev ? path.join(baseDir, "servers/fastapi") : path.join(baseDir, "resources/fastapi");
 export const nextjsDir = isDev ? path.join(baseDir, "servers/nextjs") : path.join(baseDir, "resources/nextjs");
 
-export const tempDir = app.getPath("temp")
+export const tempDir = path.join(app.getPath("temp"), "presenton")
 export const userDataDir = app.getPath("userData")
 export const downloadsDir = app.getPath("downloads")
 export const userConfigPath = path.join(userDataDir, "userConfig.json")
