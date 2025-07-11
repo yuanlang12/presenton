037defa6e5ac5ba4458c72819af84669d86ed7f0
sauravniraula <developmentsaurav@gmail.com>
2025-07-12 00:21:04 +0545

    fix(fastapi): removes check same thead for psql and mysql

--- servers/fastapi/api/services/database.py
+++ servers/fastapi/api/services/database.py
@@ -7,7 +7,11 @@ from sqlmodel import Session
 sql_url = os.getenv("SQL_URL") or "sqlite:///" + os.path.join(
     os.getenv("APP_DATA_DIRECTORY"), "fastapi.db"
 )
-sql_engine = create_engine(sql_url)
+connect_args = {}
+if "sqlite" in sql_url:
+    connect_args["check_same_thread"] = False
+
+sql_engine = create_engine(sql_url, connect_args=connect_args)
 
 
 @contextmanager
