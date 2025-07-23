9f09aaed138d37c6cd64621d050c25efc6ef6ec3
sauravniraula <developmentsaurav@gmail.com>
2025-07-23 20:44:43 +0545

    style(nextjs): make navbar backbtn look better

--- servers/nextjs/components/BackBtn.tsx
+++ servers/nextjs/components/BackBtn.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 const BackBtn = () => {
     const router = useRouter();
     return (
-        <button onClick={() => router.back()} className='bg-blue-900 rounded-full p-2'>
+        <button onClick={() => router.back()} className='bg-white-900 border border-white/20 hover:border-white/60 transition-all duration-200 rounded-full p-2'>
             <ArrowLeftIcon className="w-5 h-5 text-white" />
         </button>
     )
