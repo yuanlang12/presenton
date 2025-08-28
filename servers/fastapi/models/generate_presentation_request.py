6fa63c8ece92abf45771d646f9b98b9c8be9cce1
sauravniraula <developmentsaurav@gmail.com>
2025-08-28 18:31:57 +0545

    feat: documents support in API

--- servers/fastapi/models/generate_presentation_request.py
+++ servers/fastapi/models/generate_presentation_request.py
@@ -1,10 +1,19 @@
-from typing import Literal, Optional
+from typing import List, Literal, Optional
 from pydantic import BaseModel, Field
 
 
 class GeneratePresentationRequest(BaseModel):
     prompt: str = Field(..., description="The prompt for generating the presentation")
     n_slides: int = Field(default=8, description="Number of slides to generate")
-    language: str = Field(default="English", description="Language for the presentation")
-    template: str = Field(default="general", description="Template to use for the presentation")
-    export_as: Literal["pptx", "pdf"] = Field(default="pptx", description="Export format")
+    language: str = Field(
+        default="English", description="Language for the presentation"
+    )
+    template: str = Field(
+        default="general", description="Template to use for the presentation"
+    )
+    files: Optional[List[str]] = Field(
+        default=None, description="Files to use for the presentation"
+    )
+    export_as: Literal["pptx", "pdf"] = Field(
+        default="pptx", description="Export format"
+    )
